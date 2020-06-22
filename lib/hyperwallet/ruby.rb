3682ccf6cf2869c1f3edac82ae8199aff162118b
Jordan Trevino <jordan.trevino@gmail.com>
2020-06-22 16:01:00 -0500

    Latest

---
+++ lib/hyperwallet/ruby.rb
@@ -0,0 +1,8 @@
+require "hyperwallet/ruby/version"
+
+module Hyperwallet
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/hyperwallet/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Hyperwallet::Ruby do
+  it "has a version number" do
+    expect(Hyperwallet::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
