ae236c1ee2e9cd5e024f8a967f6f5912cd1f70b0
Jordan Trevino <jordan.trevino@gmail.com>
2020-07-17 11:29:35 -0500

    Create new payment

--- lib/hyperwallet/resources/payment.rb
+++ lib/hyperwallet/resources/payment.rb
@@ -1,4 +1,56 @@
-class Hyperwallet::Resources::Payment
+module Hyperwallet
+  module Resources
+    class Payment < Hyperwallet::Resources::Base
+      ENDPOINT = 'payments'
 
+      attr_accessor :attributes, :body
 
+      def initialize(**args)
+        @attributes = args
+        validate_fields
+      end
+
+      def create
+        connector.post(resource: ENDPOINT, payload: prepare_create_payload.to_json)
+        handle_response
+      end
+
+      def show(token:)
+      end
+
+      def update(token:)
+      end
+
+      private
+
+      def validate_fields
+        # validate elements
+      end
+
+      def prepare_create_payload
+        {
+          amount:           attributes[:amount],
+          clientPaymentId:  attributes[:client_payment_id],
+          currency:         "USD",
+          destinationToken: attributes[:destination_token],
+          programToken:     attributes[:program_token],
+          purpose:          attributes[:purpose]
+        }
+      end
+
+      def handle_response(key: nil)
+        return connector.errors unless success?
+        @body = if key
+          connector.body[key]
+        else
+          connector.body
+        end
+      end
+
+      def success?
+        connector.response.success?
+      end
+    end
+  end
 end
+
