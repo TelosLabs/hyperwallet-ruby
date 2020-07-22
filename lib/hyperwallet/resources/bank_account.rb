283c1129c803a7cee4ec562fd1fe19da37cf58be
Jordan Trevino <jordan.trevino@gmail.com>
2020-07-22 15:02:42 -0500

    Refactors to dry code

--- lib/hyperwallet/resources/bank_account.rb
+++ lib/hyperwallet/resources/bank_account.rb
@@ -4,22 +4,13 @@ module Hyperwallet
 
       ENDPOINT        = 'users'
 
-
-      private 
-
-      def validate_fields
-        # validate_date -> must be a valid date in YYYY-MM-DD format
-        # vaidate_program_token -> You must provide a valid program token (for a program which you have rights to
-      end
-
       class << self
         def method_endpoint
-          'venmo-accounts'
+          'bank-accounts'
         end
-
-        # def prepare_venmo_account_create_payload(attributes)
-        # end
       end
+
     end
+    
   end
 end
--- lib/hyperwallet/resources/venmo_account.rb
+++ lib/hyperwallet/resources/venmo_account.rb
@@ -4,19 +4,13 @@ module Hyperwallet
 
       ENDPOINT        = 'users'
 
-
-      private 
-
-      def validate_fields
-        # validate_date -> must be a valid date in YYYY-MM-DD format
-        # vaidate_program_token -> You must provide a valid program token (for a program which you have rights to
-      end
-
       class << self
         def method_endpoint
           'bank-accounts'
         end
       end
+
     end
+    
   end
 end
