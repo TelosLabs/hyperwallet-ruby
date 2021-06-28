15b0282f191acb482e055afc6b871885ccfc52ff
Diego Moreno <diegodm96@hotmail.com>
2021-06-28 12:35:28 -0500

    Update syntax to Rails 6.1 compliance

--- lib/hyperwallet/api/config.rb
+++ lib/hyperwallet/api/config.rb
@@ -1,6 +1,7 @@
 module Hyperwallet
   module Api
     class Config
+      cattr_accessor :api_user, :api_password, :api_mode, :proxy
 
       PRODUCTION_URL     = "https://api.paylution.com/rest"
       UAT_URL            = "https://uat-api.paylution.com/rest"
@@ -9,8 +10,6 @@ module Hyperwallet
       MODES       = [:uat, :production]
 
       class << self
-        cattr_accessor :api_user, :api_password, :api_mode, :proxy
-
         def uat?
           api_mode == :uat
         end
