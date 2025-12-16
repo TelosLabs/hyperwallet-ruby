f94450f5d9b9cc0ed99225df18af94aa132b6541
Daniel Medina <soydanielmedina@gmail.com>
2025-12-15 23:55:07 -0600

    update api version

--- lib/hyperwallet/api/config.rb
+++ lib/hyperwallet/api/config.rb
@@ -5,7 +5,7 @@ module Hyperwallet
       PRODUCTION_URL     = "https://api.paylution.com/rest"
       UAT_URL            = "https://uat-api.paylution.com/rest"
 
-      API_VERSION = "/v4/users"
+      API_VERSION = "/v4"
       MODES       = [:uat, :production]
 
       class << self
