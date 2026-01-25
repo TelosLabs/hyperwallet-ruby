7534eba13e36d27284d02e0f73b35a7777607201
Daniel Medina <soydanielmedina@gmail.com>
2026-01-25 00:20:48 -0600

    improve spacing

--- lib/hyperwallet/api/config.rb
+++ lib/hyperwallet/api/config.rb
@@ -2,11 +2,11 @@ module Hyperwallet
   module Api
     class Config
 
-      PRODUCTION_URL     = "https://api.paylution.com/rest"
-      UAT_URL            = "https://uat-api.paylution.com/rest"
+      PRODUCTION_URL         = "https://api.paylution.com/rest"
+      UAT_URL                = "https://uat-api.paylution.com/rest"
       PRODUCTION_API_VERSION = "/v3"
-      UAT_API_VERSION = "/v4"
-      MODES              = [:uat, :production]
+      UAT_API_VERSION        = "/v4"
+      MODES                  = [:uat, :production]
 
       class << self
         attr_accessor :api_user, :api_password, :api_mode, :proxy
